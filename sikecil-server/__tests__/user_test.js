28b452c1729d4168a6c444b7d2d79e75e67b8267
Ngurah Wira <ngurahwira27@gmail.com>
2023-11-24 00:23:23 +0700

    testing

--- sikecil-server/__tests__/user_test.js
+++ sikecil-server/__tests__/user_test.js
@@ -59,219 +59,162 @@ describe("POST /login - error", () => {
   });
 });
 
-// describe("POST /login - error", () => {
-//   it("password null ", async () => {
-//     const body = {
-//       email: "alice@example.com",
-//       password: null,
-//     };
-//     const response = await request(app).post("/login").send(body);
+describe("POST /login - error", () => {
+  it("password null ", async () => {
+    const body = {
+      email: "johndoe@example.com",
+      password: null,
+    };
+    const response = await request(app).post("/login").send(body);
 
-//     expect(response.status).toBe(401);
-//     expect(response.body).toMatchObject(expect.any(Object));
-//   });
-// });
+    expect(response.status).toBe(401);
+    expect(response.body).toMatchObject(expect.any(Object));
+  });
+});
 
-// describe("POST /login - error", () => {
-//   it("wrong email", async () => {
-//     const body = {
-//       email: "ce@example.com",
-//       password: "SecretPass123",
-//     };
-//     const response = await request(app).post("/login").send(body);
+describe("POST /login - error", () => {
+  it("wrong email", async () => {
+    const body = {
+      email: "johasdasdasndoe@example.com",
+      password: "password123",
+    };
+    const response = await request(app).post("/login").send(body);
 
-//     expect(response.status).toBe(401);
-//     expect(response.body).toMatchObject(expect.any(Object));
-//   });
-// });
+    expect(response.status).toBe(401);
+    expect(response.body).toMatchObject(expect.any(Object));
+  });
+});
 
-// describe("POST /login - error", () => {
-//   it("password not match", async () => {
-//     const body = {
-//       email: "alice@example.com",
-//       password: "Pass123",
-//     };
-//     const response = await request(app).post("/login").send(body);
+describe("POST /login - error", () => {
+  it("password not match", async () => {
+    const body = {
+      email: "johndoe@example.com",
+      password: "1231password123",
+    };
+    const response = await request(app).post("/login").send(body);
 
-//     expect(response.status).toBe(401);
-//     expect(response.body).toMatchObject(expect.any(Object));
-//   });
-// });
+    expect(response.status).toBe(401);
+    expect(response.body).toMatchObject(expect.any(Object));
+  });
+});
 
 // //! Register -----------
-// describe("POST /Add user - success", () => {
-//   it("add user ", async () => {
-//     const body = {
-//       username: "ngurah123",
-//       email: "asd@getMaxListeners.com",
-//       password: "123asd",
-//       role: "Staff",
-//       phoneNumber: 10212,
-//       address: "bali",
-//     };
-//     const response = await request(app)
-//       .post("/register")
-//       .send(body)
-//       .set("authorization", `Bearer ${access_token}`);
+describe("POST /Add user - success", () => {
+  it("add user ", async () => {
+    const body = {
+      fullname: "ngurah123",
+      email: "asd@getMaxListeners.com",
+      password: "123asd",
+      role: "staff",
+      balance: 10212,
+      address: "bali",
+    };
+    const response = await request(app)
+      .post("/register")
+      .send(body)
+      .set("authorization", `Bearer ${access_token}`);
 
-//     expect(response.status).toBe(201);
-//     expect(response.body).toMatchObject(expect.any(Object));
-//   });
-// });
+    expect(response.status).toBe(201);
+    expect(response.body).toMatchObject(expect.any(Object));
+  });
+});
 
-// describe("POST /Add user - fail", () => {
-//   it("password null ", async () => {
-//     const body = {
-//       username: "ngurah123",
-//       email: "asd@getMaxListeners.com",
-//       password: null,
-//       role: "Staff",
-//       phoneNumber: 10212,
-//       address: "bali",
-//     };
-//     const response = await request(app)
-//       .post("/register")
-//       .send(body)
-//       .set("authorization", `Bearer ${access_token}`);
+describe("POST /Add user - fail", () => {
+  it("password null ", async () => {
+    const body = {
+      fullname: "ngurah123",
+      email: "asd@getMaxListeners.com",
+      password: null,
+      role: "staff",
+      balance: 10212,
+      address: "bali",
+    };
+    const response = await request(app)
+      .post("/register")
+      .send(body)
+      .set("authorization", `Bearer ${access_token}`);
 
-//     expect(response.status).toBe(400);
-//     expect(response.body).toMatchObject(expect.any(Object));
-//   });
-// });
+    expect(response.status).toBe(400);
+    expect(response.body).toMatchObject(expect.any(Object));
+  });
+});
 
-// describe("POST /Add user - fail", () => {
-//   it("password null ", async () => {
-//     const body = {
-//       username: "ngurah123",
-//       email: null,
-//       password: "123asd",
-//       role: "Staff",
-//       phoneNumber: 10212,
-//       address: "bali",
-//     };
-//     const response = await request(app)
-//       .post("/register")
-//       .send(body)
-//       .set("authorization", `Bearer ${access_token}`);
+describe("POST /Add user - fail", () => {
+  it("password null ", async () => {
+    const body = {
+      fullname: "ngurah123",
+      email: null,
+      password: "123asd",
+      role: "staff",
+      balance: 10212,
+      address: "bali",
+    };
+    const response = await request(app)
+      .post("/register")
+      .send(body)
+      .set("authorization", `Bearer ${access_token}`);
 
-//     expect(response.status).toBe(400);
-//     expect(response.body).toMatchObject(expect.any(Object));
-//   });
-// });
+    expect(response.status).toBe(400);
+    expect(response.body).toMatchObject(expect.any(Object));
+  });
+});
 
-// describe("POST /Add user - fail", () => {
-//   it("email empty ", async () => {
-//     const body = {
-//       username: "ngurah123",
-//       email: "",
-//       password: "123asd",
-//       role: "Staff",
-//       phoneNumber: 10212,
-//       address: "bali",
-//     };
-//     const response = await request(app)
-//       .post("/register")
-//       .send(body)
-//       .set("authorization", `Bearer ${access_token}`);
+describe("POST /Add user - fail", () => {
+  it("email empty ", async () => {
+    const body = {
+      fullname: "ngurah123",
+      email: "",
+      password: "123asd",
+      role: "staff",
+      balance: 10212,
+      address: "bali",
+    };
+    const response = await request(app)
+      .post("/register")
+      .send(body)
+      .set("authorization", `Bearer ${access_token}`);
 
-//     expect(response.status).toBe(400);
-//     expect(response.body).toMatchObject(expect.any(Object));
-//   });
-// });
+    expect(response.status).toBe(400);
+    expect(response.body).toMatchObject(expect.any(Object));
+  });
+});
 
-// describe("POST /Add user - fail", () => {
-//   it("password empty ", async () => {
-//     const body = {
-//       username: "ngurah123",
-//       email: "asd@getMaxListeners.com",
-//       password: "",
-//       role: "Staff",
-//       phoneNumber: 10212,
-//       address: "bali",
-//     };
-//     const response = await request(app)
-//       .post("/register")
-//       .send(body)
-//       .set("authorization", `Bearer ${access_token}`);
+describe("POST /Add user - fail", () => {
+  it("password empty ", async () => {
+    const body = {
+      fullname: "ngurah123",
+      email: "asd@getMaxListeners.com",
+      password: "",
+      role: "Staff",
+      balance: 10212,
+      address: "bali",
+    };
+    const response = await request(app)
+      .post("/register")
+      .send(body)
+      .set("authorization", `Bearer ${access_token}`);
 
-//     expect(response.status).toBe(400);
-//     expect(response.body).toMatchObject(expect.any(Object));
-//   });
-// });
+    expect(response.status).toBe(400);
+    expect(response.body).toMatchObject(expect.any(Object));
+  });
+});
 
-// describe("POST /Add user - fail", () => {
-//   it("email doublt ", async () => {
-//     const body = {
-//       username: "ngurah123",
-//       email: "bob@example.com",
-//       password: "123asd",
-//       role: "Staff",
-//       phoneNumber: 10212,
-//       address: "bali",
-//     };
-//     const response = await request(app)
-//       .post("/register")
-//       .send(body)
-//       .set("authorization", `Bearer ${access_token}`);
+describe("POST /Add user - fail", () => {
+  it("wrong format email ", async () => {
+    const body = {
+      fullname: "ngurah123",
+      email: "example.com",
+      password: "123asd",
+      role: "Staff",
+      balance: 10212,
+      address: "bali",
+    };
+    const response = await request(app)
+      .post("/register")
+      .send(body)
+      .set("authorization", `Bearer ${access_token}`);
 
-//     expect(response.status).toBe(400);
-//     expect(response.body).toMatchObject(expect.any(Object));
-//   });
-// });
-
-// describe("POST /Add user - fail", () => {
-//   it("wrong format email ", async () => {
-//     const body = {
-//       username: "ngurah123",
-//       email: "example.com",
-//       password: "123asd",
-//       role: "Staff",
-//       phoneNumber: 10212,
-//       address: "bali",
-//     };
-//     const response = await request(app)
-//       .post("/register")
-//       .send(body)
-//       .set("authorization", `Bearer ${access_token}`);
-
-//     expect(response.status).toBe(400);
-//     expect(response.body).toMatchObject(expect.any(Object));
-//   });
-// });
-
-// describe("POST /Add user - fail", () => {
-//   it("does not contain tokens", async () => {
-//     const body = {
-//       username: "ngurah123",
-//       email: "asd@getMaxListeners.com",
-//       password: "123asd",
-//       role: "Staff",
-//       phoneNumber: 10212,
-//       address: "bali",
-//     };
-//     const response = await request(app).post("/register").send(body);
-
-//     expect(response.status).toBe(401);
-//     expect(response.body).toMatchObject(expect.any(Object));
-//   });
-// });
-
-// describe("POST /Add user - fail", () => {
-//   it("wrong token", async () => {
-//     const body = {
-//       username: "ngurah123",
-//       email: "asd@getMaxListeners.com",
-//       password: "123asd",
-//       role: "Staff",
-//       phoneNumber: 10212,
-//       address: "bali",
-//     };
-//     const response = await request(app)
-//       .post("/register")
-//       .send(body)
-//       .set("authorization", `Bearer ${access_token}jhg`);
-
-//     expect(response.status).toBe(401);
-//     expect(response.body).toMatchObject(expect.any(Object));
-//   });
-// });
+    expect(response.status).toBe(400);
+    expect(response.body).toMatchObject(expect.any(Object));
+  });
+});
